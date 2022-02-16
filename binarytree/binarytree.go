b52fef64fe39e8b2a2dd7c85ce24f37a4c50d902
matt-d-nelson <98720000+matt-d-nelson@users.noreply.github.com>
2022-02-16 14:55:57 -0600

    Additional Functions

--- binarytree/binarytree.go
+++ binarytree/binarytree.go
@@ -1,5 +1,10 @@
 package binarytree
 
+import (
+	"fmt"
+	"strconv"
+)
+
 type Node struct {
 	Value int
 	Left  *Node
@@ -41,4 +46,102 @@ func (n *Node) Insert(v int) {
 			n.Right.Insert(v)
 		}
 	}
+}
+
+func (n Node) String() string {
+	return fmt.Sprintf("%v", n.Value)
+}
+
+func (bt *BinaryTree) Path(v int) string {
+	msg := "{"
+	curr := bt.Root
+	next := bt.Root
+	for ; curr != nil && curr.Value != v; curr = next {
+
+		if msg != "{" {
+			msg += ", "
+		}
+		if v < curr.Value {
+			msg += "L"
+			next = curr.Left
+		} else if v > curr.Value {
+			msg += "R"
+			next = curr.Right
+		}
+	}
+	if curr == nil {
+		msg = "{Value not found"
+	} else {
+		msg += ", " + curr.String()
+	}
+	msg += "}"
+	return msg
+}
+
+func (bt *BinaryTree) Max() string {
+	msg := "{"
+	for curr := bt.Root; curr != nil; curr = curr.Right {
+		if curr.Right == nil {
+			msg += curr.String()
+		}
+	}
+	msg += "}"
+	return msg
+}
+
+func (bt *BinaryTree) Min() string {
+	msg := "{"
+	for curr := bt.Root; curr != nil; curr = curr.Left {
+		if curr.Left == nil {
+			msg += curr.String()
+		}
+	}
+	msg += "}"
+	return msg
+}
+
+func (n *Node) Reverse() {
+	if n == nil {
+		return
+	} else {
+		n.Left.Reverse()
+		n.Right.Reverse()
+
+		temp := n.Left
+		n.Left = n.Right
+		n.Right = temp
+	}
+}
+
+func (n *Node) Size() int {
+	if n == nil {
+		return 0
+	} else {
+		return (n.Left.Size() + n.Right.Size() + 1)
+	}
+}
+
+func (bt *BinaryTree) Size() string {
+	s := bt.Root.Size()
+	return "{Size = " + strconv.Itoa(s) + "}"
+}
+
+func (n *Node) MaxDepth() int {
+	if n == nil {
+		return 0
+	} else {
+		left := n.Left.MaxDepth() + 1
+		right := n.Right.MaxDepth() + 1
+
+		if left > right {
+			return left
+		} else {
+			return right
+		}
+	}
+}
+
+func (bt *BinaryTree) MaxDepth() string {
+	s := bt.Root.MaxDepth()
+	return "{Max depth = " + strconv.Itoa(s) + "}"
 }
--- main.go
+++ main.go
@@ -1,1 +1,30 @@
 package main
+
+import (
+	"fmt"
+
+	"github.com/matt-d-nelson/binarytree/binarytree"
+)
+
+func main() {
+	var bt binarytree.BinaryTree
+	bt.Add(10)
+	bt.Add(5)
+	bt.Add(4)
+	bt.Add(7)
+	bt.Add(17)
+	bt.Add(15)
+	bt.Add(3)
+
+	fmt.Println(bt.Path(3))
+	fmt.Println(bt.Path(18))
+
+	bt.Root.Reverse()
+	fmt.Println(bt.Path(3))
+
+	bt.Root.Reverse()
+	fmt.Println(bt.Path(3))
+	fmt.Println(bt.Size())
+	fmt.Println(bt.Max())
+	fmt.Println(bt.MaxDepth())
+}
